3efcf4a60250bbf4f00cd42f4730dc0dbf52f10e
Andrew Reilly <andrewdrx@live.com>
2024-11-19 08:49:12 -0500

    Fix return value of helper functions for IOptionEnumerableError.

--- src/Enumerations.Options/Enumerations/OptionsFunctions/OptionEnumerableError.cs
+++ src/Enumerations.Options/Enumerations/OptionsFunctions/OptionEnumerableError.cs
@@ -3,17 +3,18 @@ namespace ReillyDigital.Enumerations;
 public static partial class OptionsFunctions
 {
 	/// <inheritdoc cref="IOptionEnumerable{}.Error()" />
-	public static IError<TValue> OptionEnumerableError<TValue>() => IOptionEnumerable<TValue>.Error();
+	public static IOptionEnumerableError<TValue> OptionEnumerableError<TValue>() => IOptionEnumerable<TValue>.Error();
 
 	/// <inheritdoc cref="IOptionEnumerable{}.Error(Exception)" />
-	public static IError<TValue> OptionEnumerableError<TValue>(Exception value) =>
+	public static IOptionEnumerableError<TValue> OptionEnumerableError<TValue>(Exception value) =>
 		IOptionEnumerable<TValue>.Error(value);
 
 	/// <inheritdoc cref="IOptionEnumerable{}.Error(string, Exception?)" />
-	public static IError<TValue> OptionEnumerableError<TValue>(string message, Exception? innerException = null) =>
-		IOptionEnumerable<TValue>.Error(message, innerException);
+	public static IOptionEnumerableError<TValue> OptionEnumerableError<TValue>(
+		string message, Exception? innerException = null
+	) => IOptionEnumerable<TValue>.Error(message, innerException);
 
 	/// <inheritdoc cref="IOptionEnumerable{TValue, TError}.Error(TError)" />
-	public static IError<TValue, TError> OptionEnumerableError<TValue, TError>(TError value) =>
+	public static IOptionEnumerableError<TValue, TError> OptionEnumerableError<TValue, TError>(TError value) =>
 		IOptionEnumerable<TValue, TError>.Error(value);
 }
