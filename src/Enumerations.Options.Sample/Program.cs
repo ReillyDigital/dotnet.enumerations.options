d1d06a6086e5a69e7f73fbab4caa2024f9bddf2e
Andrew Reilly <andrewdrx@live.com>
2026-03-01 10:51:24 -0500

    Move samples project and improve output w/ titles for each scenario and breaks inbetween.

--- src/Enumerations.Options.Sample/Program.cs
+++
@@ -1,5 +0,0 @@
-SimpleOptionScenario.Run();
-BoxedOptionScenario.Run();
-CovariantOptionScenario.Run();
-StreamScenario.Run();
-BusScenario.Run();
---
+++ src/Enumerations.Options.Samples/Program.cs
@@ -0,0 +1,19 @@
+Console.WriteLine("=== SimpleOptionScenario ===");
+SimpleOptionScenario.Run();
+Console.WriteLine();
+
+Console.WriteLine("=== BoxedOptionScenario ===");
+BoxedOptionScenario.Run();
+Console.WriteLine();
+
+Console.WriteLine("=== CovariantOptionScenario ===");
+CovariantOptionScenario.Run();
+Console.WriteLine();
+
+Console.WriteLine("=== StreamScenario ===");
+StreamScenario.Run();
+Console.WriteLine();
+
+Console.WriteLine("=== BusScenario ===");
+BusScenario.Run();
+Console.WriteLine();
